0e88048cd2d05370cb50d09d02cbab895b249caa
omarram <omarramadneh@gmail.com>
2015-07-22 13:36:25 +0300

    User new stabby lambda syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
-	scope :newest_first, lambda { order("posts.created_at DESC") }
+	scope :newest_first, -> { order("posts.created_at DESC") }
 end
